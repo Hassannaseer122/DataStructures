f71f46fa4e5708b2e2e271bdc9de0b69dc11f13f
HammadMaqbool <hammad_mqbl@outlook.com>
2021-09-11 12:45:53 +0500

    First Programming Insertion operation on Data Structure Array.

--- Array_Insert_Operation_MethodOne.cpp
+++ Array_Insert_Operation_MethodOne.cpp
@@ -1,12 +1,26 @@
+/*						Code for the Insertion Operation on array
+						For Detail Lectures Please Vsit & Subscribe
+					channel : https://www.youtube.com/SoftwareEngineering
+*/
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	int A[4];
-	A[0] = 55;
-	A[1] = 255;
+	/*int A[3];
+	A[0] = 5;
+	A[1] = 55;
 	A[2] = 56;
-	A[3] = 64;
-    //output:
-    cout<<A[2];
+	A[3] = 58;
+	cout<<A[0];*/
+	
+	//Method two using For Loop and taking user input
+	int B[3];
+	for(int i=0; i<=3; i++)
+	{
+		cout<<"Enter Element for the index of "<<i<<" of Array ";
+		cin>>B[i];
+	}
+	cout<<B[2];
 }
+
